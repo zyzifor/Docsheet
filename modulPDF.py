f997f979beb09852c815e3d5c8d109dd5e686978
zyzif <zyzifor@gmail.com>
2025-01-22 17:07:27 +0500

    Alpha v0.4
    Доделан modulPDF.py
    *Добавлено вывод ошибок
    **Добавлено директория под Win/Linux

--- modulPDF.py
+++ modulPDF.py
@@ -3,27 +3,30 @@ import os
 import sys
 import platform
 import requests
+from datetime import datetime
 
 def download_html(url, temp_html_file):
-    # Загружаем HTML по URL и сохраняем в файл
+    if not url.startswith(("http://", "https://")):
+        raise ValueError(f"Некорректный URL: {url}")
     response = requests.get(url)
+    if response.status_code != 200:
+        raise Exception(f"Ошибка загрузки HTML: {response.status_code}")
     with open(temp_html_file, 'w', encoding='utf-8') as f:
         f.write(response.text)
     print(f"HTML файл загружен: {temp_html_file}")
 
 def convert_html_to_pdf(html_file, output_pdf):
-    system = platform.system()
-    if system == "Windows":
+    try:
         config = pdfkit.configuration(wkhtmltopdf=r"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe")
         pdfkit.from_file(html_file, output_pdf, configuration=config)
-    elif system == "Linux":
-        pdfkit.from_file(html_file, output_pdf)
-    print(f"PDF создан: {output_pdf}")
+        print(f"PDF создан: {output_pdf}")
+    except OSError as e:
+        print(f"Ошибка при создании PDF: {e}")
 
 def delete_temp_html(temp_html_file):
     # Удаляем временный PDF
     os.remove(temp_html_file)
-    print(f"Временный pdf файл удален: {temp_html_file}")
+    print(f"Временный HTML файл удален: {temp_html_file}")
 
 def open_pdf(output_pdf):
     # Открытие PDF в стандартной программе
@@ -37,10 +40,20 @@ def open_pdf(output_pdf):
 
 if __name__ == "__main__":
     html_file = sys.argv[1] # URL HTML
-    temp_html_file = "temp_report.html"  # Временный HTML файл
-    output_pdf = "sys.argv[1].pdf"  # Путь для сохранения PDF файла
+    temp_html_file = "temp_report.html" # Временный HTML файл
 
-    download_html(sys.argv[1], temp_html_file)
+    name = "ShiftReport" #Имя отчета
+    current_date = datetime.now().strftime("%Y-%m-%d") #Дата
+    system = platform.system()
+    # Путь для сохранения PDF файла
+    if system == "Windows":
+        save_path = r"C:\dll"
+    elif system == "Linux":
+        save_path = r"/opt/Docsheet"
+    os.makedirs(save_path, exist_ok=True)
+    output_pdf = os.path.join(save_path, f"{name}_{current_date}.pdf")
+
+    download_html(html_file, temp_html_file)
     convert_html_to_pdf(temp_html_file, output_pdf)
-    delete_temp_html(temp_html_file)
     open_pdf(output_pdf)
+    delete_temp_html(temp_html_file)
